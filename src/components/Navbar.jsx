841e1f6e2d5c0258835128c58666e733f2a054ac
Debruynw <opara2218@student.babcock.edu.ng>
2025-06-30 10:15:34 +0100

    fixed import navbar path

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import api from '../../api';
+import api from '../api';
 import UsernameModal from './auth/UsernameModal';
 
 const Navbar = () => {
